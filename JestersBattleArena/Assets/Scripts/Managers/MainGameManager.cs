e858a66ba2e6fecce7006f9bf231eac6b73a7b12
Dalius <dalius.luksa1@gmail.com>
2024-01-26 18:42:40 +0200

    Added fight arena background image and made it possible to go there by pressing "Start Fight" button inside barracks

--- JestersBattleArena/Assets/Scripts/Managers/MainGameManager.cs
+++ JestersBattleArena/Assets/Scripts/Managers/MainGameManager.cs
@@ -5,6 +5,7 @@ public class MainGameManager : MonoBehaviour
     [SerializeField] private GameObject characterCreationUI;
     [SerializeField] private GameObject barracksUI;
     [SerializeField] private GameObject townUI;
+    [SerializeField] private GameObject fightArenaUI;
 
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private Player mainPlayer;
@@ -29,5 +30,11 @@ public class MainGameManager : MonoBehaviour
     {
         townUI.SetActive(true);
         barracksUI.SetActive(false);
+    }
+
+    public void StartFight()
+    {
+        barracksUI.SetActive(false);
+        fightArenaUI.SetActive(true);
     }
 }
