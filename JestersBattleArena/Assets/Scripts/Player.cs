0a1c5ece321745f0b03a068d58db78e741bcb4a5
Dalius <dalius.luksa1@gmail.com>
2024-01-26 20:43:15 +0200

    Added weights to items, fixed chanceToGive for items to be from 0 to 1, Implemented InventoryItem class for items to be stored in inventory

--- JestersBattleArena/Assets/Scripts/Player.cs
+++ JestersBattleArena/Assets/Scripts/Player.cs
@@ -1,15 +1,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class InventoryItem
+{
+    [SerializeField] private int weight;
+    [SerializeField] private List<CharacterStat> gainedStats;
+
+    public int Weight => weight;
+
+    public InventoryItem(Item newItem)
+    {
+        gainedStats = new List<CharacterStat>();
+
+        foreach (RandomStat randomStat in newItem.OptionalRandomStats)
+        {
+            var randomNumber = Random.Range(0.0f, 1.0f);
+            if (randomNumber <= randomStat.chanceToGive)
+            {
+                var randomStatNumber = Random.Range(randomStat.minValue, randomStat.maxValue + 1);
+                // Don't add it if we rolled 0
+                if (randomStatNumber == 0)
+                {
+                    continue;
+                }
+
+                gainedStats.Add(new CharacterStat() {
+                    name = randomStat.name,
+                    charStat = randomStat.stat,
+                    value = randomStatNumber
+                });
+            }
+        }
+    }
+}
+
 public class Player : MonoBehaviour
 {
+    [SerializeField] private List<InventoryItem> playerInventory;
+    [SerializeField] private int maxInventoryWeight = 30;
+
     public List<CharacterStat> CharacterStats { get; private set; }
+    public int CurrentInventoryWeight() 
+    {
+        int value = 0;
+        foreach (var item in playerInventory)
+        {
+            value += item.Weight;
+        }
+        return value;
+    }
 
     private Character characterSO;
+
+    public Item itemToAdd;
 
     public void SetupPlayer(Character newCharacterSO)
     {
         characterSO = newCharacterSO;
         CharacterStats = newCharacterSO.CharacterStats;
+
+        playerInventory = new List<InventoryItem>();
+
+        AddItemToPlayerInventory(itemToAdd);
+    }
+
+    public void AddItemToPlayerInventory(Item newItem)
+    {
+        InventoryItem newInvItem = new InventoryItem(newItem);
+        // Only add item if the player has space
+        if (maxInventoryWeight >= CurrentInventoryWeight() + newInvItem.Weight)
+        {
+            playerInventory.Add(newInvItem);
+        }
+        else
+        {
+            // We shouldn't get here because at this point we will lose the item
+            Debug.Log("Failed to add item");
+        }
     }
 }
