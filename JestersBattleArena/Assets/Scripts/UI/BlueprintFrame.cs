c27c6e095a80086117322b0d8f04f6b082e8dd4d
Dalius <dalius.luksa1@gmail.com>
2024-01-27 15:33:36 +0200

    Now you can craft items and get them

--- JestersBattleArena/Assets/Scripts/UI/BlueprintFrame.cs
+++ JestersBattleArena/Assets/Scripts/UI/BlueprintFrame.cs
@@ -1,16 +1,28 @@
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class BlueprintFrame : MonoBehaviour
+public class BlueprintFrame : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Image blueprintSprite;
     [SerializeField] private TextMeshProUGUI blueprintResourceCostText;
 
+    private MainGameManager mainGameManager = null;
+    private Item item = null;
+
+    public Item Item => item;
+
+    private void Awake()
+    {
+        mainGameManager = FindObjectOfType<MainGameManager>();
+    }
+
     public void SetupBlueprintFrame(Item item)
     {
-        Player mainPlayer = FindObjectOfType<MainGameManager>().MainPlayer;
+        this.item = item;
+        Player mainPlayer = mainGameManager.MainPlayer;
         blueprintSprite.sprite = item.Icon;
         StringBuilder newString = new StringBuilder();
         foreach (ResourceCost resource in item.ResourcesToCraft)
@@ -18,5 +30,10 @@ public class BlueprintFrame : MonoBehaviour
             newString.AppendLine($"{resource.value}/{mainPlayer.GetResourceCount(resource.resource)} {resource.name}");
         }
         blueprintResourceCostText.text = newString.ToString();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        mainGameManager.UpdateLatestSelectedWorkshopBP(this);
     }
 }
--- JestersBattleArena/Assets/Scripts/UI/BuildingUI/WorkshopUI.cs
+++ JestersBattleArena/Assets/Scripts/UI/BuildingUI/WorkshopUI.cs
@@ -5,15 +5,20 @@ using UnityEngine.UI;
 public class WorkshopUI : MonoBehaviour
 {
     [SerializeField] private Button townButton;
+    [SerializeField] private Button craftButton;
     [SerializeField] private GameObject rootContent;
     [SerializeField] private BlueprintFrame blueprintFramePrefab;
     [SerializeField] private List<Item> availableWorkshopBPs;
 
+    private MainGameManager mainGameManager = null;
     private bool wasCreated = false;
 
     private void Start()
     {
+        mainGameManager = FindObjectOfType<MainGameManager>();
+
         townButton.onClick.AddListener(OnWorkshopButtonClicked);
+        craftButton.onClick.AddListener(OnCraftButtonClicked);
 
         UpdateCraftingInventory();
         wasCreated = true;
@@ -30,11 +35,12 @@ public class WorkshopUI : MonoBehaviour
     private void OnDestroy()
     {
         townButton.onClick.RemoveListener(OnWorkshopButtonClicked);
+        craftButton.onClick.RemoveListener(OnCraftButtonClicked);
     }
 
     private void OnWorkshopButtonClicked()
     {
-        FindObjectOfType<MainGameManager>().MoveToTown();
+        mainGameManager.MoveToTown();
     }
 
     private void UpdateCraftingInventory()
@@ -48,6 +54,21 @@ public class WorkshopUI : MonoBehaviour
         {
             BlueprintFrame newBP = Instantiate(blueprintFramePrefab, rootContent.transform);
             newBP.SetupBlueprintFrame(bp);
+        }
+    }
+
+    private void OnCraftButtonClicked()
+    {
+        if (mainGameManager.MainPlayer.IsEnoughToCraft(mainGameManager.CurrentlySelectedBP.Item))
+        {
+            mainGameManager.MainPlayer.RemoveResourcesFromPlayer(mainGameManager.CurrentlySelectedBP.Item.ResourcesToCraft);
+            mainGameManager.MainPlayer.AddItemToPlayerInventory(mainGameManager.CurrentlySelectedBP.Item);
+
+            UpdateCraftingInventory();
+        }
+        else
+        {
+            Debug.Log("Not enough materials to craft an item");
         }
     }
 }
